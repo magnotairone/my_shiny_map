af885ccaef6e053d839c3c1f326341d812799ae6
magnotairone <magnotairone@gmail.com>
2025-12-23 18:15:24 -0300

    shinylive generator

--- scripts/shinylive.R
+++ scripts/shinylive.R
@@ -1,6 +1,10 @@
 options(shinylive.cache = "C:/temp/shinylive_cache")
 
+unlink("docs", recursive = TRUE, force = TRUE)
+
 shinylive::export(
-  appdir = ".",
-  destdir = paste0(getwd(), "/docs")
+  appdir = "../my_shiny_map/",
+  destdir = "docs/",
+  overwrite = TRUE
 )
+
